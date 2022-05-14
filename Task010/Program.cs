77aa85503ea82841e54853121ab2d7dfb95f270c
«Alexandra» <sashatoropova@gmail.com>
2022-05-14 19:00:34 +0700

    Добавлена задача №10

---
+++ Task010/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Введите трехзначное число: ");
+int num = int.Parse(Console.ReadLine());
+if (num > 99 && num < 1000)
+{
+    int res = num / 10 % 10;
+    Console.WriteLine(res);
+}
+else
+{
+    Console.WriteLine("Ошибка: введено некорректное значение.");
+}
+
+
+
+// Дополнительно - для тренировки попытка прописать через метод - буду рада обратной связи.
+if (num > 99 && num < 1000)
+{
+    int secNumber = FindSecNumb(num);
+    Console.WriteLine(secNumber);
+}
+else
+{
+    Console.WriteLine("Ошибка: введено некорректное значение.");
+}
+
+int FindSecNumb(int number)
+{
+    return number / 10 % 10;
+}
